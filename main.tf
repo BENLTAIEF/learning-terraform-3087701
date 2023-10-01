f19d3ebbeaffa6c32905942e10d6500d045befcb
BENLTAIEF <bechir.benltaief@gmail.com>
2023-10-01 20:47:02 +0200

    third

--- main.tf
+++ main.tf
@@ -46,8 +46,6 @@ module "vpc" {
   azs             = ["eu-west-3a"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-
   tags = {
     Terraform = "true"
     Environment = "dev"
