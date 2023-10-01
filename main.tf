2db80740a4ea96b38bb1b5b5f66345d4f77aab73
BENLTAIEF <bechir.benltaief@gmail.com>
2023-10-01 20:45:48 +0200

    second

--- main.tf
+++ main.tf
@@ -43,11 +43,10 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
+  azs             = ["eu-west-3a"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-  enable_vpn_gateway = true
 
   tags = {
     Terraform = "true"
