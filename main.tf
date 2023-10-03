00dc472b199c0cc58b021d32a941f0262a90cbd6
BENLTAIEF <bechir.benltaief@gmail.com>
2023-10-03 14:06:50 +0200

    vpc

--- main.tf
+++ main.tf
@@ -14,29 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids=[module.blog_sg.security_group_id]
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  name="blog"
-  vpc_id      =module.vpc.public_subnets[0]
-
-  ingress_rules=["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks= ["0.0.0.0/0"]
-
-  egress_rules=["all-all"]
-  egress_cidr_blocks= ["0.0.0.0/0"]
-
-}
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -50,4 +27,29 @@ module "vpc" {
     Terraform = "true"
     Environment = "dev"
   }
+}
+
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
+  vpc_security_group_ids=[module.blog_sg.security_group_id]
+  
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+
+  name    = "blog"
+  vpc_id  = module.vpc.public_subnets[0]
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+
 }
